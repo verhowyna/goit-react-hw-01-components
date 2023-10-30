c5b573e2bc95307e44dde52a8cc6d495f4857edf
verhowyna <verhowyna@ukr.net>
2023-10-30 23:18:21 +0200

    correct

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,6 +1,7 @@
 import { FriendListItem } from "../FriendListItem/FriendListItem"
 import css from './FriendList.module.css';
 
+
 export const FriendList = () => {
     return (
         <ul className={css.list}>
